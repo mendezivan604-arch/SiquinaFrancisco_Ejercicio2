892fe78c8b478b2f3be4cf0b1e3c94ebe701b8cb
Ivan mendez <mendezivan604@gmail.com>
2026-03-07 20:53:01 -0600

    Switch y programa culminado

--- SiquinaFrancisco_Ejercicio2/Program.cs
+++ SiquinaFrancisco_Ejercicio2/Program.cs
@@ -47,3 +47,87 @@ if (dia < 1 || dia > 7)
     Console.WriteLine("Error: día fuera de rango.");
 }
 
+bool acceso = false;
+string permiso = "Sin acceso";
+
+switch (rol)
+{
+    case 1: 
+        if (dia >= 1 && dia <= 5)
+        {
+            if (hora >= 7 && hora <= 18)
+            {
+                if (carnet == "S")
+                {
+                    acceso = true;
+                    permiso = "Uso académico del laboratorio";
+                }
+            }
+        }
+        break;
+
+    case 2: 
+        if (carnet == "S")
+        {
+            acceso = true;
+            permiso = "Acceso docente completo";
+        }
+        break;
+
+    case 3: 
+        if (hora >= 7 && hora <= 18)
+        {
+            acceso = true;
+            permiso = "Mantenimiento técnico";
+        }
+        else
+        {
+            if (autorizacion == "S")
+            {
+                acceso = true;
+                permiso = "Mantenimiento fuera de horario";
+            }
+        }
+        break;
+
+    case 4: 
+        if (autorizacion == "S")
+        {
+            if (acompanado == "S")
+            {
+                acceso = true;
+                permiso = "Visita supervisada";
+            }
+        }
+        break;
+
+    default:
+        Console.WriteLine("Rol inválido.");
+        return;
+}
+
+
+if (usb == "S")
+{
+    if (autorizacion == "S" && acompanado == "S")
+    {
+        
+    }
+    else
+    {
+        acceso = false;
+        permiso = "USB no permitido";
+    }
+}
+
+Console.WriteLine("RESULTADO:");
+if (acceso)
+{
+    Console.WriteLine("Acceso permitido");
+    Console.WriteLine("Nivel de permiso: " + permiso);
+}
+else
+{
+    Console.WriteLine("Acceso denegado");
+    Console.WriteLine("Nivel de permiso: " + permiso);
+}
